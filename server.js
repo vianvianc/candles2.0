982bea9d1bbba967e3ac174496b1cf11d5c075ef
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-06-20 22:22:40 -0600

    added package.json

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express')
 
 const app=express();
 
-app.get('/api/candles', (req, res => {
+app.get('/api/candles', (req, res) => {
     const candles = [
-        {id: 1, commodity: 'crude', month: 'Jan'},
-        {id: 2, commodity: 'minigold', month: 'Jan'},
-        {id: 3, commodity: 'copper', month: 'Jan'},
-        {id: 4, commodity: 'CAD', month: 'Jan'},
-        {id: 5, commodity: 'natgas', month: 'Jan'},
+        {id: 1, commodity: 'crude ', month: 'Jan'},
+        {id: 2, commodity: 'minigold ', month: 'Jan'},
+        {id: 3, commodity: 'copper ', month: 'Jan'},
+        {id: 4, commodity: 'CAD ', month: 'Jan'},
+        {id: 5, commodity: 'natgas ', month: 'Jan'},
+    ];
+    res.json(candles);
+});
 
-    ]
-}));
-
-const port = 5000;
+const port = 4000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
