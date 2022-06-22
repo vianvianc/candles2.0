d19e40a7f9b85ae31b70b5c5ead1b2697e3da7c4
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-06-22 17:51:13 -0600

    added chart class component

--- client/src/components/charts/charts.js
+++ client/src/components/charts/charts.js
@@ -1,5 +1,6 @@
 
 import './charts.css';
+import {Component} from "react";
 
 class Charts extends Component() {
     constructor() {
@@ -8,17 +9,23 @@ class Charts extends Component() {
        charts: []}
         }
     }
-    componentDidMount() {
+
+function componentDidMount() {
+
+}
+
+componentDidMount();
+{
     fetch('/api/charts')
         .then(res => res.json())
-        .then(charts => this.setState({charts}));
+        .then(charts => this.setState({charts}, () => console.log('Charts fetched..', charts)));
 }
     render() {
-
-
+    return (
         <div className="Charts">
         <h2>Commodity</h2>
         </div>
-    };
+    )
+    }
 
 export default Charts;
