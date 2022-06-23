1704fcfdccd785c635526165ed4ceafdd191aabf
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-06-23 17:10:44 -0600

    added package.json

--- client/src/components/charts/charts.js
+++ client/src/components/charts/charts.js
@@ -1,31 +1,9 @@
 
 import './charts.css';
-import {Component} from "react";
 
-class Charts extends Component() {
-    constructor() {
-       super();
-       this.state = {
-       charts: []}
-        }
-    }
 
-function componentDidMount() {
 
-}
 
-componentDidMount();
-{
-    fetch('/api/charts')
-        .then(res => res.json())
-        .then(charts => this.setState({charts}, () => console.log('Charts fetched..', charts)));
-}
-    render() {
-    return (
-        <div className="Charts">
-        <h2>Commodity</h2>
-        </div>
-    )
-    }
+
 
 export default Charts;
